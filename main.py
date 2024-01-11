60130b50d12b59b2b042be63a1d1c7fd92c3a0fd
AdamC06 <154569813+AdamC06@users.noreply.github.com>
2024-01-11 23:01:45 +0000

    Added more options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print('Welcome to Elite 101 Chatbot! Great to see you!')
 name = input('Please enter your name:')
 age = int(input(f'Nice to meet you {name}! How old are you? '))
+
 if age < 30:
   print(f'Hello and welcome, {name}, your age is {age}, congrats! Anyways, How may I help you today?')
 elif age > 30:
@@ -10,9 +11,9 @@ x = True
 
 while x == True:
   print('Please choose from the following options:')
-  print('1. Placeholder Option 1')
-  print('2. Placeholder Option 2')
-  print('3. Placeholder Option 3')
+  print('1. Whoah, you seem fun. Lets be friends, my name is Adam!')
+  print('2. Pleasure meeting you!')
+  print('3. Nice to meet you!')
   print('4. Exit the conversation')
   choice = int(input('Enter the number of your choice: '))
   if choice == 4:
